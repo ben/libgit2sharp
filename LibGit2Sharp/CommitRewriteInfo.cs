e1fb8f8b424b010d920b936412e12ff014e9a416
Ben Straub <bs@github.com>
2013-05-12 11:27:11 -0700

    Remove ability to change commit-message encoding

--- LibGit2Sharp/CommitRewriteInfo.cs
+++ LibGit2Sharp/CommitRewriteInfo.cs
@@ -19,26 +19,22 @@ namespace LibGit2Sharp
         /// The message to be used for the new commit
         /// </summary>
         public string Message { get; set; }
-        public string Encoding { get; set; }
 
-        public static CommitRewriteInfo SameAs(Commit c)
         /// <summary>
         /// Match the <see cref="Commit"/> passed in
         /// </summary>
         /// <param name="commit">The <see cref="Commit"/> whose information is to be copied</param>
         /// <returns>A new <see cref="CommitRewriteInfo"/> object that matches the info for the <paramref name="commit"/>.</returns>
+        public static CommitRewriteInfo SameAs(Commit commit)
         {
             return new CommitRewriteInfo
                 {
-                    Author = c.Author,
-                    Committer = c.Committer,
-                    Message = c.Message,
-                    Encoding = c.Encoding,
+                    Author = commit.Author,
+                    Committer = commit.Committer,
+                    Message = commit.Message,
                 };
         }
 
-        public static CommitRewriteInfo From(Commit c, Signature author = null, Signature committer = null,
-                                             string message = null, string encoding = null)
         /// <summary>
         /// Match but optionally override the <see cref="Commit"/> passed in
         /// </summary>
@@ -48,12 +44,15 @@ namespace LibGit2Sharp
         /// <param name="message">Optional override for the message</param>
         /// <returns>A new <see cref="CommitRewriteInfo"/> object that matches the info for the
         /// <paramref name="commit"/> with the optional parameters replaced..</returns>
+        public static CommitRewriteInfo From(Commit commit,
+                                             Signature author = null,
+                                             Signature committer = null,
+                                             string message = null)
         {
-            var cri = SameAs(c);
+            var cri = SameAs(commit);
             cri.Author = author ?? cri.Author;
             cri.Committer = committer ?? cri.Committer;
             cri.Message = message ?? cri.Message;
-            cri.Encoding = encoding ?? cri.Encoding;
             return cri;
         }
     }
